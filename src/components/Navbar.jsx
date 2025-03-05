52b992e6c5ccfd8b3f45bdf4d94ec869036dc9bd
Ty Reynolds <tyreynolds.dev@gmail.com>
2025-03-04 23:57:23 -0600

    logo image fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8] backdrop-blur-lg border-b border-white/10 shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-50">
-                <a href="#home"><img src="/ihvfd-logo.png" alt="IHVFD Logo" className="h-45"></img></a>
+                <a href="#home"><img src="/images/ihvfd-logo.png" alt="IHVFD Logo" className="h-45"></img></a>
 
                 <div className="w-10 h-10 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
                     &#9776;
